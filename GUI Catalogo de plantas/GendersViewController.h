552ed0f67a14cd981f5f24ec638661fd82769970
Maria del Carmen Aguilar Velez <Maki@Marias-Mac.local>
2015-04-04 14:30:33 -0700

    Prototipo - Disenho de las primeras ventanas

---
+++ GUI Catalogo de plantas/GendersViewController.h
@@ -0,0 +1,13 @@
+//
+//  GendersViewController.h
+//  GUI Catalogo de plantas
+//
+//  Created by Maria del Carmen Aguilar Velez on 3/31/15.
+//  Copyright (c) 2015 Gustavo Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GendersViewController : UIViewController
+
+@end
